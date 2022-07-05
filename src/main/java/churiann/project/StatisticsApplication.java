99066d2fb0edb6c2933188b6cbd8ba1920f8e738
Anna <churiann@fiit.cvut.cz>
2022-07-05 12:45:37 +0200

    implement StatisticsApplication and ApplicationProcess

--- src/main/java/churiann/project/StatisticsApplication.java
+++ src/main/java/churiann/project/StatisticsApplication.java
@@ -1,4 +1,35 @@
 package churiann.project;
 
+import churiann.project.business.ApplicationProcess;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class StatisticsApplication {
+
+    public static List<String> parseInput(String input) {
+        List<String> result = new ArrayList<>();
+        Scanner scanner = new Scanner(input);
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()) {
+            result.add(scanner.next());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Invalid arguments");
+            System.exit(1);
+        }
+
+        String nameOfInputFile = args[0].substring(0, args[0].length() - 1);
+        List<String> namesOfStatistics = parseInput(args[1]);
+        List<String> namesOfFormats = parseInput(args[2]);
+
+        ApplicationProcess applicationProcess = new ApplicationProcess();
+        applicationProcess.run(nameOfInputFile, namesOfStatistics, namesOfFormats);
+    }
+
 }
